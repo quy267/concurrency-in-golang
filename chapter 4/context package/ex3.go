3de30ba21282217408ee67a7d82da49ee7fce92b
quynguyen3 <quy.nguyen3@onemount.com>
2021-09-19 18:48:58 +0700

    chapter 4

---
+++ chapter 4/context package/ex3.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	ProcessRequest3("jane", "abc123")
+}
+
+type ctxKey int
+
+const (
+	ctxUserID ctxKey = iota
+	ctxAuthToken
+)
+
+func UserID(c context.Context) string {
+	return c.Value(ctxUserID).(string)
+}
+func AuthToken(c context.Context) string {
+	return c.Value(ctxAuthToken).(string)
+}
+func ProcessRequest3(userID, authToken string) {
+	ctx := context.WithValue(context.Background(), ctxUserID, userID)
+	ctx = context.WithValue(ctx, ctxAuthToken, authToken)
+	HandleResponse3(ctx)
+}
+
+func HandleResponse3(ctx context.Context) {
+	fmt.Printf(
+		"handling response for %v (auth: %v)",
+		UserID(ctx),
+		AuthToken(ctx),
+	)
+}
